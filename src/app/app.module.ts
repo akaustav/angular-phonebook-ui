74015569213b7c41d3bceae8706b7613ef354f49
akaustav <akaustav@gmail.com>
2018-03-25 01:25:53 -0700

    Adding 2 routes and 1 service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 import { MatButtonModule, MatCheckboxModule } from '@angular/material';
 
 
 import { AppComponent } from './app.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { PopularComponent } from './popular/popular.component';
 
+import { ContactsService } from './contacts.service';
+
+const routes: Routes = [
+  { path: '', component: PopularComponent },
+  { path: 'contacts', component: ContactsComponent },
+  { path: 'popular', component: PopularComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactsComponent,
+    PopularComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    RouterModule.forRoot(routes),
     MatButtonModule,
     MatCheckboxModule
   ],
-  providers: [],
+  providers: [ContactsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/popular/popular.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-popular',
+  templateUrl: './popular.component.html',
+  styleUrls: ['./popular.component.css']
+})
+export class PopularComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
