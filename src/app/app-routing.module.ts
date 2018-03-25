edc4d827b0148d1f50419bcbbaa6d87cdcf82025
akaustav <akaustav@gmail.com>
2018-03-25 15:08:11 -0700

    Breaking out the router into it's own module

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { PopularComponent } from './pages/popular/popular.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
+
+
+const routes: Routes = [
+  { path: '', redirectTo: 'popular', pathMatch: 'full' },
+  { path: 'popular', component: PopularComponent },
+  { path: 'contacts', component: ContactsComponent },
+  { path: '**', redirectTo: 'popular' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,17 @@
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
 import { MaterialModule } from './shared/material/material.module';
 
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContactsComponent } from './pages/contacts/contacts.component';
 import { PopularComponent } from './pages/popular/popular.component';
 
-/** TODO:
-  * 1. Break the router into it's own module
-  **/
 
 import { ContactsService } from './contacts.service';
 
-const routes: Routes = [
-  { path: '', component: PopularComponent },
-  { path: 'contacts', component: ContactsComponent },
-  { path: 'popular', component: PopularComponent }
-];
 
 @NgModule({
   declarations: [
@@ -30,8 +22,8 @@ const routes: Routes = [
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    RouterModule.forRoot(routes),
-    MaterialModule
+    MaterialModule,
+    AppRoutingModule
   ],
   providers: [ContactsService],
   bootstrap: [AppComponent]
