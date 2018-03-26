d814f7df50f9866547b04658f8039c1ad0bfafce
akaustav <akaustav@gmail.com>
2018-03-25 22:46:01 -0700

    Adding Tooltip to the Add Contact Button

--- src/app/shared/components/material/material.module.ts
+++ src/app/shared/components/material/material.module.ts
@@ -8,7 +8,8 @@ import {
   MatIconModule,
   MatInputModule,
   MatListModule,
-  MatSlideToggleModule
+  MatSlideToggleModule,
+  MatTooltipModule
 } from '@angular/material';
 
 @NgModule({
@@ -20,7 +21,8 @@ import {
     MatIconModule,
     MatInputModule,
     MatListModule,
-    MatSlideToggleModule
+    MatSlideToggleModule,
+    MatTooltipModule
   ],
   exports: [
     MatButtonModule,
@@ -30,7 +32,8 @@ import {
     MatIconModule,
     MatInputModule,
     MatListModule,
-    MatSlideToggleModule
+    MatSlideToggleModule,
+    MatTooltipModule
   ]
 })
 export class MaterialModule {}
