1d4ada6c0e004bad7a88f6846da5e64f2818b63e
akaustav <akaustav@gmail.com>
2018-03-25 17:14:19 -0700

    Dummy contacts service

--- src/app/shared/services/contacts.service.ts
+++ src/app/shared/services/contacts.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@angular/core';
 
+interface IContact {
+  name: string;
+  number: string;
+}
+
 @Injectable()
 export class ContactsService {
 
-  constructor() {}
+  getContacts(): IContact[] {
+    const contacts: IContact[] = [
+      { name: 'Ameet', number: '999-999-9999' },
+      { name: 'John', number: '777-777-7777' },
+      { name: 'Mary', number: '111-111-1111' },
+      { name: 'Janez', number: '444-444-4444' }
+    ];
+
+    return contacts;
+  }
 
 }
